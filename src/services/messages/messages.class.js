4edd70409939e8485b8b4f6c4c227b09744799bf
Masembe yolan <masembeyolan2@gmail.com>
2022-11-21 13:15:10 +0300

    Changing designs to static pages

--- src/services/messages/messages.class.js
+++
@@ -1,5 +0,0 @@
-const { Service } = require('feathers-sequelize');
-
-exports.Messages = class Messages extends Service {
-  
-};
--- test/services/messages.test.js
+++
@@ -1,8 +0,0 @@
-const app = require('../../src/app');
-
-describe('\'messages\' service', () => {
-  it('registered the service', () => {
-    const service = app.service('messages');
-    expect(service).toBeTruthy();
-  });
-});
