5f32071c798783cc1b39d763d03ea11fc9130af8
Dell 3453 <isaacgeorge6@gmail.com>
2022-11-21 13:27:59 +0300

    convert design to static pages

---
+++ client/src/pages/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-input outlined v-model="text" label="Outlined" />
+  <q-input outlined v-model="text" label="Outlined" />
+</template>
+
+<script>
+export default {
+  //es
+  name: "Login"
+}
+</script>
+
+<style scoped>
+
+</style>
