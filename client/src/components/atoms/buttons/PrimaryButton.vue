a456d44deba8193c4c646e004498818049a03c7e
marthafelice <akorafelicem@gmail.com>
2022-12-03 20:39:03 +0300

    buttons added

--- client/src/components/atoms/buttons/PrimaryButton.vue
+++ client/src/components/atoms/buttons/PrimaryButton.vue
@@ -9,12 +9,16 @@
 
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 #PrimaryButton{
   width: 24.75em;
   height:4.125em;
   border-radius: 31px;
   border: 1 solid var(--q-dark);
+ &:deep(.q-btn__content) {
+   line-height: 1.188em;
+   font-size: 1em;
+ }
 }
 
 </style>
