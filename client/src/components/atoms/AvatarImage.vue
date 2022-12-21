b7662b59e846dafd95ca65651ffbb002817aa138
Dell 3453 <isaacgeorge6@gmail.com>
2022-12-21 15:09:35 +0300

    started adding atoms

---
+++ client/src/components/atoms/AvatarImage.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-avatar>
+    <img src="https://cdn.quasar.dev/img/avatar.png" alt="logo">
+  </q-avatar>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ client/src/components/atoms/primaryButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-btn no-caps color="primary" label="login" id="primary-button"/>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+#primary-button{
+  width: 300px;
+  height: 45px;
+  border-radius: 4px;
+}
+</style>
