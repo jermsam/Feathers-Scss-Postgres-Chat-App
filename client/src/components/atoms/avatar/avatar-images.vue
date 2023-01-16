20db04968d9ea5e0bae10b7fa1d08487fd655987
Masembe yolan <masembeyolan2@gmail.com>
2023-01-16 09:19:19 +0300

    added an avatar atom

---
+++ client/src/components/atoms/avatar/avatar-images.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="q-pa-md q-gutter-sm">
+      <q-avatar>
+        <img src="https://cdn.quasar.dev/img/avatar.png">
+      </q-avatar>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped lang="scss">
+#avatarCopy {
+  position: absolute;
+  left: 3525px;
+  top: -371.2px;
+  width: 80px;
+  height: 80px;
+  border-radius: 40px;
+}
+
+</style>
